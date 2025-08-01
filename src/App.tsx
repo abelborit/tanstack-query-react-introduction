d28899707539cf07e8f29a7adbdc7b761d26d6d1
Abel Borit Guitton <abelboritguitton@gmail.com>
2025-08-01 17:54:40 -0500

    [add] useRandomNumber

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,37 @@
-import { useQuery } from "@tanstack/react-query";
-import "./App.css";
 import { RandomNumber } from "./components/RandomNumber";
-
-/* random number api -> https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new */
-const RANDOM_NUMBER_API =
-  "https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new";
-
-const getRandomNumber = async (): Promise<number> => {
-  console.log("[App] - getRandomNumber");
-  const response = await fetch(RANDOM_NUMBER_API).then((response) => {
-    return response.json();
-  });
-
-  return Number(response);
-};
+import { useRandomNumber } from "./hooks/useRandomNumber";
+import "./App.css";
 
 function App() {
-  const { isLoading, isFetching, isError, data, error, refetch } = useQuery({
-    queryKey: ["randomNumber"],
-    queryFn: getRandomNumber,
-    staleTime: 1000 * 60, // tiempo de que se considera "fresca" o "reciente" la petición
-    // refetchOnWindowFocus: false, // para que al salir y volver a entrar a la pestaña no se hagan re-peticiones de forma innecesaria, aunque colocando el "staleTime" de cierta forma no lo realiza porque ya se configuró para que se considere "fresca" o "reciente" la petición
-  });
+  const { randomNumberQuery } = useRandomNumber();
 
   const handleRefreshRequest = () => {
-    refetch();
+    randomNumberQuery.refetch();
   };
 
   return (
     <>
-      {isLoading || isFetching ? <h1>Cargando...</h1> : <h1>Número: {data}</h1>}
+      {randomNumberQuery.isLoading || randomNumberQuery.isFetching ? (
+        <h1>Cargando...</h1>
+      ) : (
+        <h1>Número: {randomNumberQuery.data}</h1>
+      )}
 
       <RandomNumber />
 
-      {isError ? (
+      {randomNumberQuery.isError ? (
         <>
           <br />
-          <div>{error.message}</div>
+          <div>{randomNumberQuery.error?.message}</div>
           <br />
         </>
       ) : null}
 
       <br />
-      <button onClick={handleRefreshRequest} disabled={isLoading || isFetching}>
+      <button
+        onClick={handleRefreshRequest}
+        disabled={randomNumberQuery.isLoading || randomNumberQuery.isFetching}
+      >
         Nuevo número
       </button>
     </>
---
+++ src/hooks/useRandomNumber.ts
@@ -0,0 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+
+/* random number api -> https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new */
+const RANDOM_NUMBER_API =
+  "https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new";
+
+const getRandomNumber = async (): Promise<number> => {
+  console.log("[App] - getRandomNumber");
+  const response = await fetch(RANDOM_NUMBER_API).then((response) => {
+    return response.json();
+  });
+
+  return Number(response);
+};
+
+export const useRandomNumber = () => {
+  const randomNumberQuery = useQuery({
+    queryKey: ["randomNumber"],
+    queryFn: getRandomNumber,
+    staleTime: 1000 * 60, // tiempo de que se considera "fresca" o "reciente" la petición
+    // refetchOnWindowFocus: false, // para que al salir y volver a entrar a la pestaña no se hagan re-peticiones de forma innecesaria, aunque colocando el "staleTime" de cierta forma no lo realiza porque ya se configuró para que se considere "fresca" o "reciente" la petición
+  });
+
+  return { randomNumberQuery };
+};
